7b2940254b8fc96c595f7503a92e025dbbed25b6
Bryan Velasco Pachuca <canomex017@gmail.com>
2025-10-04 08:25:38 -0600

    Creación de tablas y carga para limpieza de datos de alumnos.

---
+++ limpieza_dataset_alumnos/01_Tables.sql
@@ -0,0 +1,19 @@
+USE ING_DATOS;
+
+CREATE TABLE STUDENT_DATA_RAW (
+    nombre VARCHAR(100),
+    edad VARCHAR(50),
+    lugar_nacimiento VARCHAR(100),
+    promedio VARCHAR(50),
+    fecha_nacimiento VARCHAR(50),
+    donde_vivo VARCHAR(100),
+    trabajo VARCHAR(20),
+    donde_trabajo VARCHAR(100),
+    semestre VARCHAR(50),
+    def_ingenieria VARCHAR(150),
+    def_dato VARCHAR(200),
+    artista_favorito VARCHAR(70),
+    hobbies VARCHAR(100),
+    con_cuantos_vivo VARCHAR(50),
+    materias_favoritas VARCHAR(100)
+);
