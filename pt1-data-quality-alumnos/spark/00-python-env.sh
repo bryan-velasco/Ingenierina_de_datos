3af5a342e7be13359b9f14111795d444d884c411
Bryan Velasco Pachuca <canomex017@gmail.com>
2025-11-19 23:54:03 -0600

    se corrige scrip de ambiente.

--- pt1-data-quality-alumnos/spark/00-python-env.sh
+++ pt1-data-quality-alumnos/spark/00-python-env.sh
@@ -12,27 +12,38 @@ sudo apt install -y python3-venv
 
 VENV_NAME="venv_spark"
 
-# Crear entorno virtual
-python3 -m venv $VENV_NAME
+if [ ! -d "$VENV_NAME" ]; then
+    echo "Creando entorno virtual '$VENV_NAME'..."
+    python3 -m venv $VENV_NAME
+else
+    echo "Ya existe un entorno virtual llamado '$VENV_NAME'."
+fi
 
-# Activar entorno
+echo "Creando entorno virtual..."
+
+
 echo "Activando entorno virtual..."
+source $VENV_NAME/bin/activate
 
 echo "Actualizando pip dentro del entorno..."
 pip install --upgrade pip
 
-echo "Instalando librerías necesarias..."
+echo "Instalando librerías necesarias dentro del venv..."
 pip install pyspark
 pip install notebook ipykernel
 
-# Registrar kernel en Jupyter
 echo "Registrando kernel en Jupyter..."
 python -m ipykernel install --user --name "$VENV_NAME" --display-name "PySpark Env"
 
 echo "---------------------------------------"
 echo "Entorno virtual listo!"
-echo "Para activarlo de nuevo se usa: source venv/bin/$VENV_NAME"
 echo ""
-echo "Con el ambiente activado ya se puede ejecutar:"
-echo "    02-limpieza-spark.ipynb"
+echo "Para activarlo nuevamente usa:"
+echo "   source $VENV_NAME/bin/activate"
+echo ""
+echo "Con el ambiente activado ya se pueden usar los notebook:"
+echo "   jupyter notebook"
+echo "   jupyter lab"
+echo "   jupyter lab 01-limpieza-spark.ipynb"
 echo "---------------------------------------"
+
