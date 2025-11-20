cea64066648a17e6fd8d0460b522f285eafc51d4
Bryan Velasco Pachuca <canomex017@gmail.com>
2025-11-19 23:35:08 -0600

    Ajustes para limpieza con Spark.

--- pt1-data-quality-alumnos/spark/00-python-env.sh
+++ pt1-data-quality-alumnos/spark/00-python-env.sh
@@ -1,36 +1,38 @@
 #!/bin/bash
-# Este script debe ejecutarse al mismo nivel donde se tenga el script 02 de limpieza
-# También es necesario darle permisos con: chmod +x setup_etl.sh
+# Este script debe ejecutarse al mismo nivel donde se tenga el script ##
+# También es necesario darle permisos con: chmod +x 00-python-env.sh.sh
 # Al finalizar se contará con el ambiente virtual creado,
 # pero en caso de querer activarlo después se realizará con: source venv/bin/activate
 # Con el ambiente activado ahora si se puede ejecutar el script: python3 02-limpieza.py
 
-echo "Iniciando instalación de ambiente ..."
+echo "Iniciando instalación de ambiente..."
 
 echo "Instalando soporte para entornos virtuales (python3-venv)..."
 sudo apt install -y python3-venv
 
-if [ ! -d "venv" ]; then
-    echo "Creando entorno virtual 'venv'..."
-    python3 -m venv venv
-else
-    echo "Ya existe un entorno virtual llamado 'venv'."
-fi
+VENV_NAME="venv_spark"
 
+# Crear entorno virtual
+python3 -m venv $VENV_NAME
+
+# Activar entorno
 echo "Activando entorno virtual..."
-source venv/bin/activate
 
 echo "Actualizando pip dentro del entorno..."
 pip install --upgrade pip
 
 echo "Instalando librerías necesarias..."
-pip install pandas sqlalchemy psycopg2-binary
+pip install pyspark
+pip install notebook ipykernel
+
+# Registrar kernel en Jupyter
+echo "Registrando kernel en Jupyter..."
+python -m ipykernel install --user --name "$VENV_NAME" --display-name "PySpark Env"
 
 echo "---------------------------------------"
 echo "Entorno virtual listo!"
-echo "Para activarlo de nuevo se usa: source venv/bin/activate"
+echo "Para activarlo de nuevo se usa: source venv/bin/$VENV_NAME"
 echo ""
 echo "Con el ambiente activado ya se puede ejecutar:"
-echo "    python3 02-limpieza.py"
-echo "    python3 03-carga-postgres.py"
+echo "    02-limpieza-spark.ipynb"
 echo "---------------------------------------"
